c4edbcc31775b83acdf3d392f3241c11ca564709
Gustavo <gustavosohne38@gmail.com>
2020-06-09 19:43:28 -0300

    db

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ if (port == null || port === '') {
   port = 8000
 }
 
-const app = express()
 const DB = mongoose.connect(process.env.mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 })
+const app = express()
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
